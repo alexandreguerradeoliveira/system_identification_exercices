ffa23962b174265d1d2e01c7f8fa879a3b4fd005
alexandreguerradeoliveira <alex4212345@gmail.com>
2022-03-22 19:51:20 +0100

    corrected ex 1.3 and 1.4

--- CE1/exo3/exo3_mfile.m
+++ CE1/exo3/exo3_mfile.m
@@ -1,50 +1,66 @@
 clc,close all,clear all
 
 Te = 0.1; %[s]
-N = 40; %[-] number of points
+N = 1001; %[-] number of points
 
 tt = (0:Te:(N-1)*Te)';
-u = rand(size(tt));
 
-%plot(tt,u)
-r = zeros(size(u));
+sat_up = 0.5;
+u = rand(size(tt))*sat_up;
+%no noise system as comparison
+
+G = tf([-1 2],[1 1.85 4]);
+y_nonoise = lsim(G,u,tt);
+
+%simulate system with simulink
+simin.time = tt;
+simin.signals.values = u;
+out_sim = sim('exo3.slx');
+y = out_sim.simout.Data;
+
+% % plot the outputs
+% % hold on
+% % plot(tt,y)
+% % plot(tt,y_nonoise)
+% % legend("noisy system","noise-less system")
+
+%% deconvolution method (finite impulse response)
+K = 70;
+
+% create asymetric toeplitz matrix
+r = zeros(1,K);
 r(1) = u(1);
 T = toeplitz(u,r);
 
-%apply u to system, mesure y
+% solve least squares
+g_fir = inv((T')*(T))*((T')*y);
 
-G = tf([-1 2],[1 1.85 4]); %% change tgis
-
-y = lsim(G,u,tt);%% change tgis
-
-%simin.time = tt;
-%simin.signals.values = u;
-
-%out_sim = sim('exo3.slx');
-
-%y = out_sim.simout.Data;
-
-%plot(tt,y)
-%figure
-%hold on
-
-g = inv(T)*(y);
-hold on
-plot(tt,g)
-%figure
-
+%% ideal system (no noise, no saturation)
 sys_d = c2d(G,Te);
 [y_d,t_d] = impulse(sys_d*Te);
+
+%% part 5
+lambda = 2;
+
+r = zeros(size(u));
+r(1) = u(1);
+T_full = toeplitz(u,r); % full N*N asymetric toeplitz matrix
+
+g_reg = inv(T_full'*T_full+lambda*eye(size(T_full)))*(T_full')*y; % solve the least squares problem
+
+%% plots
+figure
+hold on
+
+plot(tt(1:K),g_fir)
 plot(t_d,y_d)
+plot(tt(1:K),g_reg(1:K))
 
-% part 5
-lambda = .01;
+title("Impulse response")
+legend("Indentified response (FIR)","Ideal response (no saturation, no noise)","Indentified response (Regularisation)")
+xlabel("time [s]")
+ylabel("impulse response [arbitrary units]")
 
-g_reg = inv(T'*T+lambda*eye(size(T)))*(T')*y;
-
-plot(tt,g_reg)
-
-legend("indentified system","sampled system","regulirised indentification")
 
 
 
--- CE1/exo4/exo4_mfile.m
+++ CE1/exo4/exo4_mfile.m
@@ -1,10 +1,8 @@
 clc,close all,clear
 %%
-
-sat_up = 0.5; % must fix the maximum amplitude to not saturate the input
+sat_up = 0.5; % must fix the maximum amplitude as to not saturate the input
 Uprbs = prbs(8,8)*sat_up;
 Te = 0.1;
-
 tt = (0:Te:(size(Uprbs,1)-1)*Te)';
 
 % pass to simulink stuct
@@ -13,63 +11,46 @@ simin.signals.values = Uprbs;
 
 % call the simulation
 out_step = sim('exo4.slx');
-
 tt_sim = out_step.simout.Time;
 y_sim = out_step.simout.Data;
 
-% R_uu = intcor(Uprbs(1:255),Uprbs(1:255));
-% R_yu = intcor(y_sim(1:255),Uprbs(1:255));
-% 
-% U_toeplitz = toeplitz(R_uu);
-% 
-% g_k = inv(U_toeplitz)*(R_yu')
-% 
-% 
-% plot(tt_sim(1:255),g_k)
-
-K = 255;
-
+% pick only the first period of the signal
+K = 2^(8)-1; 
 Uprbs = Uprbs(1:K);
 y_sim = y_sim(1:K);
 tt_sim = tt_sim(1:K);
 
-R_uu = intcor(Uprbs,Uprbs);
-R_yu = intcor(y_sim,Uprbs);
+R_uu_intcor = intcor(Uprbs,Uprbs);
+R_yu_intcor = intcor(y_sim,Uprbs);
 
-U_toeplitz = toeplitz(R_uu);
+U_toeplitz_intcor = toeplitz(R_uu_intcor);
 
-g_k = inv(U_toeplitz)*(R_yu');
-
-
+g_k_intcor = inv(U_toeplitz_intcor)*(R_yu_intcor');
 
 %% now with matlab
-
 
 R_uu_matlab = xcorr(Uprbs,Uprbs);
 R_yu_matlab = xcorr(y_sim,Uprbs);
 
-
+%keep only the positive part of the correlations functions
 R_yu_matlab = R_yu_matlab((end+1)/2:end);
 R_uu_matlab = R_uu_matlab((end+1)/2:end);
 
-U_toep_matlab =  toeplitz(R_uu_matlab);
-
-
-g_k_matlab = inv(U_toep_matlab)*R_yu_matlab;
+U_toeplitz_matlab =  toeplitz(R_uu_matlab);
+g_k_matlab = inv(U_toeplitz_matlab)*R_yu_matlab;
 
 %% exact reponse of the system
 G = tf([-1 2],[1 1.85 4]);
 G = c2d(G,Te);
 g_theory = impulse(G,tt_sim)*Te;
 
-
 %% plots
 hold on 
-plot(tt_sim,g_k)
+plot(tt_sim,g_k_intcor)
 plot(tt_sim,g_k_matlab)
 plot(tt_sim,g_theory)
 title("Impulse response g(t)")
 xlabel("discrete time k")
-legend("Impulse reponse with intcor()","Impulse reponse with xcorr()","Theoretical impulse response without noise")
+legend("Impulse reponse with intcor()","Impulse reponse with xcorr()","Theoretical impulse (no noise, no saturation)")
 
 
